fef46ef0f9dabe872cbd824d237a4802d09dbfff
AforArdi <binsbro36@gmail.com>
2026-04-09 08:15:16 +0600

    banner updated with buttons

--- src/components/TrendingApps/TrendingApps.jsx
+++ src/components/TrendingApps/TrendingApps.jsx
@@ -1,7 +1,13 @@
 const TrendingApps = () => {
     return ( 
-        <div>
-            trending apps
+        <div className="container mx-auto mt-10">
+            <div className="text-center space-y-2">
+                <h2 className="font-bold text-5xl">Trending Apps</h2>
+                <p className="text-[16px] text-[#627382]">Explore All Trending Apps on the Market developed by us</p>
+            </div>
+            <div>
+                
+            </div>
         </div>
      );
 }
--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -1,4 +1,6 @@
+import { FaAppStore } from 'react-icons/fa';
 import bannerImg from '../../assets/images/hero.png'
+import { IoLogoGooglePlaystore } from 'react-icons/io5';
 
 const Banner = () => {
     return ( 
@@ -7,6 +9,14 @@ const Banner = () => {
             <span className="text-purple-500">Productive</span> Apps
             </h1>
             <p className="text-[20px] text-[#627382] w-[60%] text-center">At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+            <div className='flex items-center gap-3'>
+                <button className='btn'>
+                    <IoLogoGooglePlaystore></IoLogoGooglePlaystore> Google Play
+                    </button>
+                <button className='btn'>
+                    <FaAppStore></FaAppStore> App Store
+                </button>
+            </div>
             <img src={bannerImg} alt="" />
         </div>
      );
